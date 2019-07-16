1c89b8818fe91a6032dcec318cd98e2f453401b8
suganthicj <52270901+suganthicj@users.noreply.github.com>
2019-07-16 02:30:24 -0700

    Create susuvj.py

---
+++ susuvj.py
@@ -0,0 +1,11 @@
+str1 = "hello"
+str2 = 'hello'
+
+print "id(str1) =", hex(id(str1))
+print "id(str2) =", hex(id(str2))
+
+# The above code will give the output as.
+#
+# id(str1) = 0x1587f00
+# id(str2) = 0x1587f00
+#
